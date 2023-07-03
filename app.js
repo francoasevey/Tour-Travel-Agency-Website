50cab1735e36253232cd29a106e8c4882eea776e
francoromeo <francoromeo5012@gmail.com>
2023-07-02 23:52:32 -0300

    js 5

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ Array.from(document.getElementsByTagName('input')).forEach((e,i)=>{
         if (e.value.length > 0) {
             document.getElementsByClassName('bi-caret-down-fill')[i].style.transform = "rotate(180deg)";
         } else {
-            /*document.getElementsByClassName('bi')[i].style.transform = "rotate(0deg)";*/
+            document.getElementsByClassName('bi-caret-down-fill')[i].style.transform = "rotate(0deg)";
         }
     })
 })
