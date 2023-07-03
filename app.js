30a5f382e50f40cc471f66f58f7c59a7d6545f6b
francoromeo <francoromeo5012@gmail.com>
2023-07-02 23:32:10 -0300

    js

---
+++ app.js
@@ -0,0 +1,9 @@
+Array.from(document.getElementsByTagName('input')).forEach((e,i)=>{
+    e.addEventListener('keyup', (el)=>{
+        if (e.value.length) {
+            
+        } else {
+            
+        }
+    })
+})
